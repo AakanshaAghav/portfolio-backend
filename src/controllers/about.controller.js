5cfa10a5a893f6e64a7428261e01348f12731835
AakanshaAghav <aakanshaaghav4@gmail.com>
2025-12-26 10:52:24 +0530

    Support image upload

--- src/controllers/about.controller.js
+++ src/controllers/about.controller.js
@@ -1,4 +1,6 @@
 const pool = require("../config/db");
+const fs = require("fs");
+const path = require("path");
 
 // GET about (public)
 exports.getAbout = async (req, res) => {
@@ -15,13 +17,29 @@ exports.getAbout = async (req, res) => {
 
 // UPDATE about (admin)
 exports.updateAbout = async (req, res) => {
-  const { content, image_url, resume_url } = req.body;
+  const { content, resume_url } = req.body;
+  let image_url = null;
 
   try {
+    if(req.file) {
+      const uploadDir = "uploads/about";
+      if(!fs.existsSync(uploadDir)){
+        fs.mkdirSync(uploadDir, {recursive: true});
+      }
+
+      const fileName = Date.now() + "-" + req.file.originalname;
+      
+      const filePath = path.join(uploadDir, fileName);
+
+      fs.writeFileSync(filePath, req.file.buffer);
+
+      image_url = `${req.protocol}://${req.get("host")}/${filePath}`;
+    }
+
     const result = await pool.query(
       `UPDATE about
        SET content=$1,
-           image_url=$2,
+           image_url=COALESCE($2, image_url),
            resume_url=$3,
            updated_at=NOW()
        RETURNING *`,
--- src/routes/about.routes.js
+++ src/routes/about.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/auth.middleware");
+const upload = require("../config/multer");
 const about = require("../controllers/about.controller");
 
 router.get("/", about.getAbout);
-router.put("/", auth, about.updateAbout);
+router.put("/", auth,  upload.single("image"), about.updateAbout);
 
 module.exports = router;
